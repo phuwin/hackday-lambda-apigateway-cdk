4923408fcce90731c58b36465f655d8852f387ff
Phu Nguyen <phu.nguyen@aller.com>
2021-02-12 13:05:46 +0200

    feat: added lambda and apigateway functions

--- lib/hackday-lambda-apigateway-stack.ts
+++ lib/hackday-lambda-apigateway-stack.ts
@@ -1,9 +1,27 @@
+import * as core from "@aws-cdk/core";
 import * as cdk from '@aws-cdk/core';
-
+import * as apigateway from "@aws-cdk/aws-apigateway";
+import * as lambda from "@aws-cdk/aws-lambda";
 export class HackdayLambdaApigatewayStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor(scope: core.Construct, id: string) {
+    super(scope, id);
 
-    // The code that defines your stack goes here
+
+    const handler = new lambda.Function(this, "helloworldHandler", {
+      runtime: lambda.Runtime.NODEJS_10_X, // So we can use async in helloworld.js
+      code: lambda.Code.fromAsset("resources"),
+      handler: "main.handler",
+    });
+
+    const api = new apigateway.RestApi(this, "helloworlds-api", {
+      restApiName: "helloworld Service",
+      description: "This service serves helloworlds."
+    });
+
+    const gethelloworldsIntegration = new apigateway.LambdaIntegration(handler, {
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+    });
+
+    api.root.addMethod("GET", gethelloworldsIntegration); // GET /
   }
 }
